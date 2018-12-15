493e1d44c2b8c7084f57dc146d6a96c4053253e6
katherinegriffin <katherinegriffin@katherines-air.lumos.net>
2018-12-15 15:53:59 -0500

    Added methods to LA class

--- LA_Calendar/src/LA.java
+++ LA_Calendar/src/LA.java
@@ -1,4 +1,29 @@
 
 public class LA {
 
+	private String name;
+	private int hours;
+	private int workedHours;
+	
+	public LA (String name, int hours){
+		this.name= name;
+		this.hours = hours;
+		this.workedHours = 0;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public int getTotalHours(){
+		return hours;
+	}
+	
+	public int getWorkedHours(){
+		return workedHours;
+	}
+	
+	public void setHoursLeft(){
+		workedHours = 
+	}
 }
