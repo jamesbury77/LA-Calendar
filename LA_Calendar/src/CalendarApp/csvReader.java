7622197ebfbca571ab3f19cefb08145cc660f8f5
katherinegriffin <katherinegriffin@katherines-air.lumos.net>
2018-12-16 14:16:08 -0500

    added ore to csv reader and the time csv file

--- LA_Calendar/src/CalendarApp/csvReader.java
+++ LA_Calendar/src/CalendarApp/csvReader.java
@@ -1,9 +1,12 @@
 package CalendarApp;
 
+import java.util.List;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class csvReader {
@@ -13,7 +16,8 @@ public class csvReader {
         BufferedReader br = null;
         String line = "";
         String cvsSplitBy = ",";
-        HashMap<Integer, LA[]> avail = new HashMap<Integer, LA[]>();
+        List<LA> avail = new ArrayList<LA>();
+        HashMap<String, Integer>time = setTimeValues();
         
         try {
 
@@ -24,12 +28,16 @@ public class csvReader {
                 String[] resources = line.split(cvsSplitBy);
 
                 LA la = new LA(resources[0], Integer.parseInt(resources[1]));
-                
-                for (int i = 2; i < resources.length; i++)
-                	System.out.println(resources[i] + '\n');
-
-            }
-
+               
+                for (int i = 2; i < resources.length; i++){
+                	if (time.containsKey(resources[i])){
+                		avail.add(time.get(resources[i]), la);
+                		System.out.printl);
+                		}
+                		
+                	}
+                }
+            Arrays.toString(avail.toArray());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -44,7 +52,43 @@ public class csvReader {
             }
 
     }
-
 		
 	}
+	
+	
+	public static HashMap<String, Integer> setTimeValues(){
+		HashMap<String, Integer> time = new HashMap<String, Integer>();
+		String csvFile = "time.csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+        int i = 0;
+        
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+            	String[] set = line.split(cvsSplitBy);
+                time.put(set[0], i);
+                i++;
+               
+                }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+	}
+        return time;
+}
+	
+
 }
